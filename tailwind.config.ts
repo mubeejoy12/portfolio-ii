6c8edcc6510c59141a9979c90ad38e85ec9c6a84
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-12-03 15:18:46 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black:{
+          DEFAULT: "#000000",
+          100: "#000319"
+        }
+        
       },
     },
   },
